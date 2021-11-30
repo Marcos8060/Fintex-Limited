dfe52f4cd470f8a93b373cb7659b85e2a6253da1
Marcos Ochieng <marcosgav80@gmail.com>
2021-12-01 02:24:41 +0300

    added dynamic images and details in the gallery section

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // GET VARIABLES FOR ALL INPUT FIELDS
+const locationInput = document.getElementById('location');
+const priceInput = document.getElementById('price');
+const roomInput = document.getElementById('room');
+const productItems = document.querySelector('.productItems');
+
+products.forEach((product)=>{
+    productItems.innerHTML += `
+            <div class="col-md-3">
+            <div class="card mb-3">
+            <img
+                src="${product.imgSrc}"
+                class="card-img-top img-fluid"
+                alt="..."
+            />
+            <div class="card-body">
+                <div class="content">
+                <h5 class="card-title price">ksh${product.price}</h5>
+                <img class="img-fluid"
+                    src="https://img.icons8.com/material-outlined/50/fa314a/filled-like.png"
+                />
+                </div>
+                <div class="info">
+                <p class="card-text">${product.rooms}Bdrm</p>
+                <p class="card-text">${product.size}sqm</p>
+                <p class="card-text">Gated</p>
+                </div>
+                <div class="location">
+                <div>
+                    <span>Estate</span><br />
+                    <span>${product.location}</span>
+                </div>
+                <div>
+                    <span>AGent</span><br />
+                    <span>${product.agent}</span>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    `
+})
--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 const products = [
     {
         id: 0,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/img1.jpg',
         location: 'Runda',
         price: '3,200,000',
         rooms: 3,
@@ -10,7 +10,7 @@ const products = [
     },
     {
         id: 1,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/11.jpg',
         location: 'Kiambu Road',
         price: '1,100,000',
         rooms: 2,
@@ -20,7 +20,7 @@ const products = [
     },
     {
         id: 2,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/16.jpeg',
         location: 'Kilimani',
         price: '4,400,000',
         rooms: 4,
@@ -29,7 +29,7 @@ const products = [
     },
     {
         id: 3,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/12.jpg',
         location: 'Westlands',
         price: '1,200,000',
         rooms: 3 ,
@@ -38,7 +38,7 @@ const products = [
     },
     {
         id: 4,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/img5.jpg',
         location: 'Karen',
         price: '6,000,000',
         rooms: 5,
@@ -47,7 +47,7 @@ const products = [
     },
     {
         id: 5,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/img6.jpg',
         location: 'Runda',
         price: '7,300,000',
         rooms: 5,
@@ -56,7 +56,7 @@ const products = [
     },
     {
         id: 6,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/img7.jpg',
         location: 'Lavington',
         price: '8,400,000',
         rooms: 6,
@@ -65,7 +65,7 @@ const products = [
     },
     {
         id: 7,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/img8.jpg',
         location: 'kitengela',
         price: '2,200,000',
         rooms: 3,
@@ -74,7 +74,7 @@ const products = [
     },
     {
         id: 8,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/img9.jpg',
         location: 'Runda',
         price: '7,700,000',
         rooms: 7,
@@ -83,7 +83,7 @@ const products = [
     },
     {
         id: 9,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/img10.jpg',
         location: 'Karen',
         price: '6,100,000',
         rooms: 3,
@@ -92,7 +92,7 @@ const products = [
     },
     {
         id: 10,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/img11.jpg',
         location: 'Kiambu Road',
         price: '5,500,000',
         rooms: 2,
@@ -101,7 +101,7 @@ const products = [
     },
     {
         id: 11,
-        imgSrc : 'https://www.rocketmortgage.com/resources-cmsassets/RocketMortgage.com/Article_Images/Large_Images/TypesOfHomes/types-of-homes-hero.jpg',
+        imgSrc : '/Images/img12.jpg',
         location: 'Kilimani',
         price: '4,000,000',
         rooms: 1,
@@ -110,7 +110,7 @@ const products = [
     },
     {
         id: 12,
-        imgSrc : 'https://i.ytimg.com/vi/1_cSm6A5dlw/hqdefault.jpg',
+        imgSrc : '/Images/10.jpg',
         location: 'Spring Valley',
         price: '12,000,000',
         rooms: 5,
@@ -119,7 +119,7 @@ const products = [
     },
     {
         id: 13,
-        imgSrc : 'https://www.peponirealestate.com/wp-content/uploads/2015/02/IMG_2980-600x300.jpg',
+        imgSrc : '/Images/15.jpeg',
         location: 'Muthaiga',
         price: '15,000,000',
         rooms: 6,
@@ -137,119 +137,11 @@ const products = [
     },
     {
         id: 15,
-        imgSrc : 'https://images.kenyapropertycentre.com/properties/images/5338/05f34ccd9ee990-dik-dik-gardens-redevelopment-opportunity-detached-bungalows-for-sale-kileleshwa-nairobi.jpg',
+        imgSrc : '/Images/9.jpg',
         location: 'Kilimani',
         price: '25,000,000',
         rooms: 6,
         agent: 'Erick',
         size: 232
-    },
-    {
-        id: 16,
-        imgSrc : 'https://daebakinvestments.com/wp-content/uploads/2019/06/64782914_2566504523368000_7699916281918521344_n.jpg',
-        location: 'Muthaiga',
-        price: '73,000,000',
-        rooms: 8,
-        agent: 'Vanessa',
-        size: 240
-    },
-    {
-        id: 17,
-        imgSrc : 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/f7/68/ae/the-cottage-gigiri.jpg?w=900&h=-1&s=1',
-        location: 'Westlands',
-        price: '63,000,000',
-        rooms: 7,
-        agent: 'Marcos',
-        size: 210
-    },
-    {
-        id: 18,
-        imgSrc : 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/35/2f/a0/orchid-homes.jpg?w=900&h=-1&s=1',
-        location: 'Kiambu road',
-        price: '43,000,000',
-        rooms: 6,
-        agent: 'George',
-        size: 112
-    },
-    {
-        id: 19,
-        imgSrc : 'https://i1.wp.com/www.standard-property.com/wp-content/uploads/2017/04/1.jpg?fit=960%2C720&ssl=1',
-        location: 'Muthaiga',
-        price: '33,000,000',
-        rooms: 5,
-        agent: 'Erick',
-        size: 123
-    },
-    {
-        id: 20,
-        imgSrc : 'https://www.standard-property.com/wp-content/uploads/2017/11/Kerarapond-2.jpg',
-        location: 'Gigiri',
-        price: '21,000,000',
-        rooms: 4,
-        agent: 'Vanessa',
-        size: 147
-    },
-    {
-        id: 21,
-        imgSrc : 'https://cdn06.allafrica.com/download/pic/main/main/csiid/00530300:9e87b093fb2c028239f86df792d65734:arc614x376:w735:us1.jpg',
-        location: 'Lavington',
-        price: '34,000,000',
-        rooms: 4,
-        agent: 'Marcos',
-        size: 48
-    },
-    {
-        id: 22,
-        imgSrc : 'https://www.nwhm.com/sites/default/files/2021-03/epic_IMG_854x650px.jpg',
-        location: 'Lavington',
-        price: '83,000,000',
-        rooms: 9,
-        agent: 'George',
-        size: 252
-    },
-    {
-        id: 23,
-        imgSrc : 'https://www.realestatedatabase.net/PropertyPhotos/464/210920181125250096_210920181131207165.jpeg',
-        location: 'Westlands',
-        price: '56,000,000',
-        rooms: 5,
-        agent: 'Erick',
-        size: 183
-    },
-    {
-        id: 24,
-        imgSrc : 'https://willstonehomes.co.ke/wp-content/uploads/2021/11/IMG_1121-scaled.jpg',
-        location: 'Karen',
-        price: '47,000,000',
-        rooms: 4,
-        agent: 'Vanessa',
-        size: 136
-    },
-    {
-        id: 25,
-        imgSrc : 'https://robbreport.com/wp-content/uploads/2020/03/1011beverlydr_65.jpg?w=1000',
-        location: 'Spring Valley',
-        price: '77,000,000',
-        rooms: 6,
-        agent: 'Marcos',
-        size: 250
-    },
-    {
-        id: 26,
-        imgSrc : 'https://i.ytimg.com/vi/4QlH5EjzXdk/maxresdefault.jpg',
-        location: 'Spring Valley',
-        price: '73,000,000',
-        rooms: 5,
-        agent: 'George',
-        size: 253
-    },
-    {
-        id: 27,
-        imgSrc : 'https://i.ytimg.com/vi/4QlH5EjzXdk/maxresdefault.jpg',
-        location: 'Spring Valley',
-        price: '73,000,000',
-        rooms: 5,
-        agent: 'George',
-        size: 253
     },
 ]

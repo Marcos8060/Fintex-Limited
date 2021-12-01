0a43cf40c5fbe416f12c835ed544e3b52548d529
GeorgeM101 <george.mboya@student.moringaschool.com>
2021-12-01 18:23:40 +0300

    addded dates alert

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var names = document.getElementById("name");
 var email = document.getElementById("email");
 var number = document.getElementById("number");
+var dates = document.getElementById("dates")
 
 function formInput(){
-    if(names.value == "")
+if(names.value == ''){
     alert("Please enter your name")
+}else{
+    alert(`Thank you ${names.value} for reaching out to us. We will contact you on the ${dates.value}`);
+}
 }

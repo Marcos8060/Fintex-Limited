613466656ccb11079d7a9263ce7c53c4db55180f
GeorgeM101 <george.mboya@student.moringaschool.com>
2021-12-01 17:45:27 +0300

    Entered date input

---
+++ main.js
@@ -0,0 +1,8 @@
+var names = document.getElementById("name");
+var email = document.getElementById("email");
+var number = document.getElementById("number");
+
+function formInput(){
+    if(names.value == "")
+    alert("Please enter your name")
+}
